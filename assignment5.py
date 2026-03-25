347e52ab0dee24d634786f08ffcc61ef935000db
pranalid052007-alt <pranalid052007@gmail.com>
2026-03-25 10:37:38 +0530

    Create assignment5.py for string manipulation tasks
    Added assignment5.py with string and math functions examples.

---
+++ assignment5.py
@@ -0,0 +1,19 @@
+#To study built-in functions of string and math packages
+#Assignment 5
+#1.WAP to calculate length of string 
+a=input ("enter a string:")
+print("length of string is:",len(a))
+
+
+#2.WAP to make string from first two and last teo characters from given string
+b=input ("enter a string:")
+c=b[:2]
+d=b[-2:]
+e=c+d
+print(e)
+
+
+#WAP to cocatenate two strings
+x="Hello"
+y="World!"
+print(x+y)
